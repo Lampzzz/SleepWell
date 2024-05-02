b61671009b69f5b3cc23010351477237e2e1d332
Kozuki Oden <147463361+Lampzzz@users.noreply.github.com>
2024-05-02 11:07:24 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5174",
     "http://localhost:5173",
-    "https://sleepwell-frontend.vercel.app",
+    "https://sleepwell-frontend.vercel.app/",
   ],
   credentials: true,
 };
