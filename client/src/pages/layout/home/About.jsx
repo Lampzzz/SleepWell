2daf88dc0106eb9da384d9aefec17312e6dccc51
Kozuki Oden <147463361+Lampzzz@users.noreply.github.com>
2024-05-06 18:47:48 +0800

    Update About.jsx

--- client/src/pages/layout/home/About.jsx
+++ client/src/pages/layout/home/About.jsx
@@ -1,5 +1,5 @@
 import ButtonLink from "../../../components/button/ButtonLink";
-import aboutImg from "../../../assets/image/about--img.jpg";
+import aboutImg from "../../../assets/image/about--img.png";
 
 const About = () => {
   const imgSize = {
@@ -11,7 +11,7 @@ const About = () => {
     <div className="container-fluid py-5 bg-white " id="about">
       <div className="row justify-content-center align-items-center">
         <div className="col-md-6 col-lg-5 order-md-1 order-1 text-center mb-3 mb-lg-0">
-          <img src={aboutImg} alt="about us image" style={imgSize} />
+          <img src={aboutImg} alt="about us image img-fluid" />
         </div>
         <div className="col-md-6 col-lg-5 order-2 order-md-2 px-5 px-md-0">
           <h2 className="mb-3">Welcome to SleepWell</h2>
