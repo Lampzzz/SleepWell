bd3c5e8f6aa64af9bb3b0309f0de639f2a3cfd43
Kozuki Oden <147463361+Lampzzz@users.noreply.github.com>
2024-05-06 20:11:52 +0800

    Update FooterLogo.jsx

--- client/src/components/footer/FooterLogo.jsx
+++ client/src/components/footer/FooterLogo.jsx
@@ -1,12 +1,9 @@
+import logo from "../../assets/image/logo.png";
+
 const FooterLogo = () => {
   return (
     <div>
-      <img
-        src="src/assets/image/logo.png"
-        id="logo"
-        alt="Logo"
-        className="mb-3"
-      />
+      <img src={logo} id="logo" alt="Logo" className="mb-3" />
       <p className="mb-0 text-black">
         Experience an elevated sleep journey through state-of-the-art
         monitoring, promoting holistic well-being.
