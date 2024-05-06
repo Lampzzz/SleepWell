c6c1e074963f1a0a8136e99bb0fd1d3b7314b150
Kozuki Oden <147463361+Lampzzz@users.noreply.github.com>
2024-05-06 12:03:19 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:5174", ["http://localhost:5173"]],
+    origin: ["http://localhost:5173", "https://sleepwell-client.vercel.apps"],
     credentials: true,
   })
 );
